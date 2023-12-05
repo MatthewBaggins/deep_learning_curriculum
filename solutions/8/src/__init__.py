4939aa6dcc3226ecea6d84bba8f02c2e8361f98e
Mateusz Bagiński <m.baginski@optimumpareto.com>
2023-12-05 08:26:18 +0100

    some progress on 8 interp

--- solutions/8/src/__init__.py
+++ solutions/8/src/__init__.py
@@ -1,3 +1,4 @@
 from .dataset import Dataset
 from .model import CNN
 from .training import TrainingHistory, train
+from .utils import load_latest
---
+++ solutions/8/src/utils.py
@@ -0,0 +1,34 @@
+import os
+from pathlib import Path
+
+import torch as t
+
+from . import CNN, TrainingHistory
+
+def load_model(path: Path) -> CNN:
+    assert path.exists()
+    assert str(path).endswith(".pt")
+    with open(path, "rb") as f:
+        model = t.load(f)
+    assert isinstance(model, CNN)
+    return model
+
+def load_latest() -> tuple[CNN, TrainingHistory]:
+    filenames = sorted(os.listdir("results"))
+    latest_model_path = "results" / Path(
+        next(
+            fname 
+            for fname in filenames
+            if fname.startswith("model") and fname.endswith(".pt")
+        )
+    )
+    latest_th_path = "results" / Path(
+        next(
+            fname
+            for fname in filenames
+            if fname.startswith("th") and fname.endswith(".json")
+        )
+    )
+    model = load_model(latest_model_path)
+    th = TrainingHistory.load_from_json(latest_th_path)
+    return model, th
